768e58a7062206b60d1b2eb6ada550e4931730e4
aminat oladeji <olabisioladeji3@gmail.com>
2025-08-19 15:24:29 +0100

    i added Week 3

---
+++ Week_3/Task 8_Two.py
@@ -0,0 +1,12 @@
+# Task 2
+# Comment the code
+name = input("Enter your name: ")              # input is use to collect data
+age = int(input("Enter your age: "))           # input is use to collect data 
+score = int(input("Enter your test score: "))  # input is use to collect data
+eligibility = (age < 18) and (score > 70)      # using comparison to show the boolean value of the "eligibility" data
+print(f"Candidate: {name}\nAge: {age}\nScore: {score}\nEligible: {eligibility}")     # display the data item and data
+
+# Federal Gvernment Scholarship Key Eligibitity
+Citizenshipn= (input("Are you a citizenship of Nigeria"))
+Enrollment = (input("Are you a full_time "))
+
---
+++ Week_3/Working_with_control_flow.py
@@ -0,0 +1,5 @@
+# Conditional Statement
+# If Statement
+age = 20
+if age >= 18:
+    print("You are eligible to vote")
